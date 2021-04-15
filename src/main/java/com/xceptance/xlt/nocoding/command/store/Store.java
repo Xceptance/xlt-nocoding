abcb1dd78611ac57d93fd914044b9a6e8f86b38f
Michael Meyling <14306799+m-31@users.noreply.github.com>
2021-04-15 20:01:23 +0200

    Support emptying of dynamic variable pool via yaml.

--- src/main/java/com/xceptance/xlt/nocoding/command/store/Store.java
+++ src/main/java/com/xceptance/xlt/nocoding/command/store/Store.java
@@ -2,9 +2,11 @@ package com.xceptance.xlt.nocoding.command.store;
 
 import com.xceptance.xlt.api.util.XltLogger;
 import com.xceptance.xlt.nocoding.command.Command;
+import com.xceptance.xlt.nocoding.util.Constants;
 import com.xceptance.xlt.nocoding.util.context.Context;
 import com.xceptance.xlt.nocoding.util.resolver.VariableResolver;
 import com.xceptance.xlt.nocoding.util.storage.DataStorage;
+import com.xceptance.xlt.nocoding.util.storage.unit.unique.UniqueStorage;
 
 /**
  * A StoreItem consist of a {@link #variableName} and a {@link #value}, which gets stored in the {@link DataStorage} in
@@ -37,15 +39,27 @@ public class Store implements Command
     /**
      * Executes the {@link Store} by resolving the values and then storing the {@link #variableName} with its
      * {@link #value} in {@link Context#getVariables()}.
+     * If {@link #getVariableName()} is {@link Constants#STORE} and {@link #getValue()} is {@link Constants#DELETE},
+     * all variables are deleted.
      */
     @Override
     public void execute(final Context<?> context)
     {
         // Resolve values
         resolveValues(context);
-        // Store the variable
-        context.getVariables().store(variableName, value);
-        XltLogger.runTimeLogger.info("Added Variable: " + variableName + " : " + value);
+        // Get the appropriate storage
+        final UniqueStorage storage = context.getVariables();
+        // If the variable is not "Store" and the value is not "Delete"
+        if (!variableName.equals(Constants.STORE) && !value.equals(Constants.DELETE))
+        {
+            // Store the variable
+            storage.store(variableName, value);
+            XltLogger.runTimeLogger.info("Added Variable: " + variableName + " : " + value);
+        }
+        else {
+            storage.clear();
+            XltLogger.runTimeLogger.info("Removed all Variables");
+        }
     }
 
     public String getVariableName()
--- src/main/java/com/xceptance/xlt/nocoding/parser/yaml/command/store/StoreParser.java
+++ src/main/java/com/xceptance/xlt/nocoding/parser/yaml/command/store/StoreParser.java
@@ -3,6 +3,8 @@ package com.xceptance.xlt.nocoding.parser.yaml.command.store;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.xceptance.xlt.nocoding.command.storeDefault.StoreDefaultParameter;
+import com.xceptance.xlt.nocoding.util.Constants;
 import org.yaml.snakeyaml.error.Mark;
 import org.yaml.snakeyaml.nodes.Node;
 
@@ -11,6 +13,9 @@ import com.xceptance.xlt.api.util.XltLogger;
 import com.xceptance.xlt.nocoding.command.Command;
 import com.xceptance.xlt.nocoding.command.store.Store;
 import com.xceptance.xlt.nocoding.parser.yaml.YamlParserUtils;
+import org.yaml.snakeyaml.nodes.ScalarNode;
+import org.yaml.snakeyaml.nodes.SequenceNode;
+import org.yaml.snakeyaml.parser.ParserException;
 
 /**
  * The class for parsing store items.
@@ -22,7 +27,7 @@ public class StoreParser
 
     /**
      * Parses the store item to a list of {@link Command}s.
-     * 
+     *
      * @param context
      *            The {@link Mark} of the surrounding {@link Node}/context.
      * @param storeNode
@@ -33,15 +38,34 @@ public class StoreParser
     {
         final List<Command> scriptItems = new ArrayList<>();
 
-        // Convert the node to a list of NameValuePair so it retains its order
-        final List<NameValuePair> storeItems = YamlParserUtils.getSequenceNodeAsNameValuePair(context, storeNode);
-
-        for (final NameValuePair storeItem : storeItems)
+        if (storeNode instanceof ScalarNode)
         {
-            // Add a new StoreItem to the scriptItems
-            scriptItems.add(new Store(storeItem.getName(), storeItem.getValue()));
-            // Log the added item
-            XltLogger.runTimeLogger.debug("Added " + storeItem.getName() + "=" + storeItem.getValue() + " to StoreItems");
+            final String value = YamlParserUtils.transformScalarNodeToString(context, storeNode);
+            if (value.equals(Constants.DELETE))
+            {
+                scriptItems.add(new Store(Constants.STORE, Constants.DELETE));
+            }
+            else
+            {
+                throw new ParserException("Node", context,
+                        " contains " + value + " but it must either contain an array or " + Constants.DELETE,
+                        storeNode.getStartMark());
+            }
+        }
+        else if (storeNode instanceof SequenceNode) {
+            // Convert the node to a list of NameValuePair so it retains its order
+            final List<NameValuePair> storeItems = YamlParserUtils.getSequenceNodeAsNameValuePair(context, storeNode);
+
+            for (final NameValuePair storeItem : storeItems) {
+                // Add a new StoreItem to the scriptItems
+                scriptItems.add(new Store(storeItem.getName(), storeItem.getValue()));
+                // Log the added item
+                XltLogger.runTimeLogger.debug("Added " + storeItem.getName() + "=" + storeItem.getValue() + " to StoreItems");
+            }
+        } else {
+            throw new ParserException("Node", context,
+                    " must either contain an array or " + Constants.DELETE,
+                    storeNode.getStartMark());
         }
 
         // Return all StoreItems
